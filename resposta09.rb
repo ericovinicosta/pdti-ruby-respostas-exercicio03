dcc2bdfaad2b8acf595795e740432f6c8ca3f834
Erico Costa <prof.ericocosta@gmail.com>
2020-10-06 18:25:50 -0300

    Terceiro commit da atividade 03

--- resposta09.rb
+++ resposta09.rb
@@ -3,6 +3,10 @@ Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 =end
 
 for i in 1..50
-  teste_impar = i % 2
-  puts "#{i}" if teste_impar
+  teste_impar = i.to_i % 2
+  puts "#{i}" if teste_impar == 1
 end
+
+#for i in 1..50
+#   puts "#{i}" if i.odd
+#end
--- resposta10.rb
+++ resposta10.rb
@@ -1,3 +1,17 @@
 =begin
 Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
 =end
+print 'Entre com o número inicial: '
+numero_inicial = gets.to_i
+print 'Entre com o último número: '
+ultimo_numero = gets.to_i
+
+if numero_inicial < ultimo_numero
+  for i in numero_inicial+1...ultimo_numero
+    puts "#{i}"
+  end
+else
+  for i in (numero_inicial-1...ultimo_numero).step(-1)
+    puts "#{i}"
+  end
+end
